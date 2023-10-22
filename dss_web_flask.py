774a4fb9f452eca244ffd3a917821fe7dde8a6c5
EthanSeok <champ9162@naver.com>
2023-10-22 21:30:24 +0900

    remove redis

--- dss_web_flask.py
+++ dss_web_flask.py
@@ -1,10 +1,8 @@
 import io
-import redis
 from flask import Flask, request, render_template, Response
 import time
 
-app = Flask(__name__)
-r = redis.Redis(host='localhost', port=6379, db=0)
+app = Flask(__name)
 
 def generate_graph(graph_type, date_today_month, date_today_day):
     date_today_month = str(date_today_month).zfill(2)
@@ -30,15 +28,8 @@ def main(graph_type, date_today_month, date_today_day):
     img_path = generate_graph(graph_type, date_today_month, date_today_day)
 
     if img_path:
-        cache_key = f"{graph_type}:{date_today_month}:{date_today_day}"
-
-        cached_image = r.get(cache_key)
-        if cached_image:
-            return Response(cached_image, content_type="image/png")
-
         with open(img_path, "rb") as image_file:
             image_data = image_file.read()
-            r.set(cache_key, image_data)
 
         time.sleep(10)
         return Response(image_data, content_type="image/png")
@@ -46,4 +37,4 @@ def main(graph_type, date_today_month, date_today_day):
     return "Invalid graph type or date"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8002)
